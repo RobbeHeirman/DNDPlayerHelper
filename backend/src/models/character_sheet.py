c3b7dbb7cc9edf14727a34b24cd6ac6d507dbaf1
robbe <rheirman@overseas.be>
2023-10-19 22:26:47 +0200

    Switched to ModelSQL

--- backend/src/models/character_sheet.py
+++ backend/src/models/character_sheet.py
@@ -1,31 +1,41 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlmodel import Field, SQLModel
 
-import src.database
-from src.models.char_class import Class
-from src.models.race import Race
+from models.util.decorators import make_fields_optional
 
 
-class CharacterSheet(src.database.Base):
-    __tablename__ = "character_sheet"
+class CharacterSheetBase(SQLModel):
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    character_name: Mapped[str] = mapped_column(default="")
+    def __new__(cls, *args, **kwargs):
+        instance = super.__new__(*args, **kwargs)
+        print("called?")
+        return instance
 
     # Core info
-    lvl: Mapped[int] = mapped_column(default=1)
-    experience: Mapped[int] = mapped_column(default=0)
+    id: int = Field(primary_key=True, default=None)
+    character_name: str = Field(default="")
+
+    lvl: int = Field(default=1)
+    experience: int = Field(default=0)
 
     # Abilities
-    strength: Mapped[int] = mapped_column(default=10)
-    dexterity: Mapped[int] = mapped_column(default=10)
-    intelligence: Mapped[int] = mapped_column(default=10)
-    wisdom: Mapped[int] = mapped_column(default=10)
-    charisma: Mapped[int] = mapped_column(default=10)
+    strength: int = Field(default=10)
+    dexterity: int = Field(default=10)
+    intelligence: int = Field(default=10)
+    wisdom: int = Field(default=10)
+    charisma: int = Field(default=10)
+
+
+@make_fields_optional
+class CharacterSheetPostSchema(CharacterSheetBase):
+    pass
+
+
+class CharacterSheetTable(CharacterSheetBase, table=True):
+    __tablename__ = "character_sheet"
 
     # Core info foreign models
-    class_id: Mapped[int] = mapped_column(ForeignKey(f"{Class.__tablename__}.id"))
-    race_id: Mapped[int] = mapped_column(ForeignKey(f"{Race.__tablename__}.id"))
+    # class_id: int = Field(ForeignKey(f"{Class.__tablename__}.id"))
+    # race_id: int = Field(ForeignKey(f"{Race.__tablename__}.id"))
 
     def __repr__(self):
         return str(self.id)
